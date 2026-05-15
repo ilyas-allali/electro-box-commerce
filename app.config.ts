ca267eafe6f4c412714aae94b97d91ee47f44524
ilallali <allaliilyas4@gmail.com>
2026-05-15 18:19:54 +0200

    s

--- app.config.ts
+++ app.config.ts
@@ -1,1 +1,1 @@
-import { defineConfig } from '@tanstack/react-start/config';\nexport default defineConfig({ server: { preset: 'vercel' } });
+import { defineConfig } from '@tanstack/react-start/config';\nexport default defineConfig({ server: { preset: 'node-server' } });
